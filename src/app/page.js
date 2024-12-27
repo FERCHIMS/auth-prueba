b82011201f62f031044dde9a46151eccae9eec00
FERCHIMS <gr.egfredderick@gmail.com>
2024-12-27 10:58:09 -0300

    pruebas de errores

--- src/app/page.js
+++ src/app/page.js
@@ -1,20 +1,21 @@
 
 /* import { useUser } from '@auth0/nextjs-auth0/client'; */
-import CompPrueba from '@/components/Comp-prueba/CompPrueba';
-import { getSession } from '@auth0/nextjs-auth0';
+/* import CompPrueba from '@/components/Comp-prueba/CompPrueba';
+import { getSession } from '@auth0/nextjs-auth0'; */
 
 import Image from 'next/image';
 import Link from 'next/link';
 export default async function Home() {
   /* const { user } = useUser() */
 
-  const session = await getSession()
+  /* const session = await getSession()
   const user = session?.user
-  console.log("user", user);
+  console.log("user", user); */
 
   return (
     <>
-      {user ? (
+    <h2>ESTO ES UN HOME</h2>
+      {/* {user ? (
         <div>
           <h2>{user.name} </h2>
           <Link href="/api/auth/logout" >LOGOUT </Link>
@@ -24,24 +25,7 @@ export default async function Home() {
         <div>
           <Link href="/api/auth/login" >LOGIN </Link>
         </div>
-      )}
+      )} */}
     </>
   )
-
 }
-
-/* if (user) {
-    return (
-      <div>
-        <h2>{user.name} </h2>
-        <Link href="/api/auth/logout">Logout</Link>
-      </div>
-    ); 
-  } */
-
-{/* <div>
-      <Link href="/api/auth/login">Login</Link>
-      <div>
-        <Image src="" width={500} height={500} alt='foto 1' />
-      </div>
-    </div> */}
