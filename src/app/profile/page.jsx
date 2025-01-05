24fa6815fd300899f9496c765ce530b3a862d28a
FERCHIMS <gr.egfredderick@gmail.com>
2025-01-04 21:13:59 -0300

    probando cosas

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -5,27 +5,31 @@ import ProfileServer from "./components/user-server";
 import { getSession } from '@auth0/nextjs-auth0';
 import { redirect } from "next/navigation";
 import Image from "next/image";
+import Link from "next/link";
 
 const Profile = async () => {
   const session = await getSession();
   const user = session?.user || null;
-  if (!user) {
+  console.log("USER NAME:", user);
+  
+  /* if (!user) {
     redirect("/")
-  }
+  } */
   return (
     <div>
       {!user ? (
-        <a href="/api/auth/login">Login</a>
+        <Link href="/api/auth/login">Login</Link>
       ) : (
-        <a href="/api/auth/logout">Logout</a>
+        <Link href="/api/auth/logout">Logout</Link>
       )}
       {user? (
         <div>
           <Image src="https://iili.io/2nNcnDP.png" width={500} height={500} alt="foto profile" />
+          <h2 className="text-[4rem] font-bold tracking-tight">{user.name} </h2>
         </div>
       ): (
         <div>
-          
+          <h2>NO HAY USUARIO</h2>
         </div>
       )}
     
